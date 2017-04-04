bf4f3219f21be30257500ca86c88f0cf76c91cb6
Artem <Artemur18@gmail.com>
2017-04-04 16:23:10 +0300

    conf store update

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,13 +2,20 @@ import { createStore, applyMiddleware } from 'redux';
 import combineReducers from '../reducers';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { loadState, saveState} from './localStorage'
 
-export default function configureStore(initialState) {
+export default function configureStore() {
 	const logger  = createLogger();
+
+	const localState = loadState();
+
 	const store = createStore(
 		combineReducers,
-	    initialState,
+        localState,
 	    applyMiddleware(thunk, logger));
+	store.subscribe(() => {
+        saveState(store.getState())
+	})
 	
 	return store;
 }
