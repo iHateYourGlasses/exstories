2fccf0c7f792b88a6362a04119e7eb1f33ea588e
Artem <Artemur18@gmail.com>
2017-05-11 17:09:10 +0300

    home page stub

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export default class Home extends Component {
+  render() {
+    return (
+        <div className='col-xs-12 text-center home'>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/8p1uLKYAwEw" frameborder="0" allowfullscreen></iframe>
+        </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ import MyUserPage from './containers/MyUserPage'
 import UserPage from './containers/UserPage'
 import Menu from './containers/Menu'
 import Story from './containers/Story'
+import Home from './components/Home'
 
 const store = configureStore();
 
@@ -36,6 +37,8 @@ render(
           <Route path='/stories/:storyType' exact={true} component={Stories}/>
 
           <Route path='/story/:storyId' exact={true} component={Story}/>
+
+          <Route exact path="/" component={Home}/>
 
         </div>
       </Router>
